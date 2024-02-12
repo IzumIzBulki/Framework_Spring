b15a936cdb0feb49eba5293ba183ec91f217617b
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-02-12 14:16:33 +0300

    User.java

---
+++ FS_Homework_3/src/main/java/com/example /springBootRest/model/User.java
@@ -0,0 +1,45 @@
+package com.example.springBootRest.model;
+
+/**
+ * Модель пользователя
+ */
+public class User {
+    private String name; // наименование
+    private int age; // возраст
+    private String email; // почта
+
+    public User() {
+    }
+
+    public User(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // region getters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    // endregion
+}
