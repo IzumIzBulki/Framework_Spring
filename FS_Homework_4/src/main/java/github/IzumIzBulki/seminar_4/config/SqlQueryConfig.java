53625a15d88da56bfa0a11ff594fae5134f56164
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-02-19 13:42:14 +0300

    Create SqlQueryConfig.java

---
+++ FS_Homework_4/src/main/java/github/IzumIzBulki/seminar_4/config/SqlQueryConfig.java
@@ -0,0 +1,34 @@
+package github.IzumIzBulki.seminar_4.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Конфигурация запросов к БД из properties-файла
+ */
+@Getter
+@Component
+@PropertySource("classpath:query.properties")
+public class SqlQueryConfig {
+
+    @Value("${get.all}")
+    private String getAll;
+
+    @Value("${get.by.id}")
+    private String getById;
+
+    @Value("${get.by.name}")
+    private String getByName;
+
+    @Value("${user.insert}")
+    private String insert;
+
+    @Value("${user.update}")
+    private String update;
+
+    @Value("${user.delete}")
+    private String delete;
+
+}
