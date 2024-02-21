4d09e87b6cb088dda9526004513c0f45c467ea8c
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-02-21 11:41:00 +0300

    Create TaskStatus.java

---
+++ FS_Homework_5/src/main/java/com/geekbrains/IzumIzBulki/backlog/enums/TaskStatus.java
@@ -0,0 +1,11 @@
+package com.geekbrains.IzumIzBulki.backlog.enums;
+/**
+ * Перечисление TaskStatus определяет возможные статусы задачи.
+ * Задача может быть в статусе:
+ * NOT_STARTED - "не начата",
+ * IN_PROGRESS - "в процессе"
+ * COMPLETED - "завершена".
+ */
+public enum TaskStatus {
+    NOT_STARTED, IN_PROGRESS, COMPLETED;
+}
