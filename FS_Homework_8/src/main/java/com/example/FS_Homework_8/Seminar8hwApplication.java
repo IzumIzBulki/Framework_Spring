bd6c33a4931936b2e6e97ae6fe5197b80a0ec784
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-03-04 14:39:04 +0300

    Create Seminar8hwApplication.java

---
+++ FS_Homework_8/src/main/java/com/example/FS_Homework_8/Seminar8hwApplication.java
@@ -0,0 +1,15 @@
+package com.example.FS_Homework_8;
+
+import com.example.FS_Homework_8.services.NoteService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Seminar8hwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar8hwApplication.class, args);
+	}
+
+}
