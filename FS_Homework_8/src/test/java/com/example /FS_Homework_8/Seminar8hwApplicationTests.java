f14b4e0e82722ebb0d20e5637f45cd69f6122636
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-03-04 14:43:26 +0300

    Create Seminar8hwApplicationTests.java

---
+++ FS_Homework_8/src/test/java/com/example /FS_Homework_8/Seminar8hwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FS_Homework_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar8hwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
