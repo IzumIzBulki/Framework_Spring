206b05bef7aa71c40d2acb7f747d8eb3821fda4e
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-03-07 15:10:10 +0300

    Create GetaweyApplicationTests.java

---
+++ FS_Homework_9/Gateway/src/test/java/ru/IzumIzBulki/getawey/GetaweyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.IzumIzBulki.getawey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetaweyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
