8ceee8e8ff41cf324c59b504a4bcd3514339c446
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-03-07 13:00:12 +0300

    Create Student.java

---
+++ FS_Homework_9/WebServer/src/main/java/ru/IzumIzBulki/server/model/Student.java
@@ -0,0 +1,20 @@
+package ru.IzumIzBulki.server.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String groupe;
+    Double averageScore;
+}
