4db21782f4cfa0d430b86f0cf2991839e41ba60c
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-03-07 13:02:18 +0300

    Create Students.java

---
+++ FS_Homework_9/WebServer/src/main/java/ru/IzumIzBulki/server/model/Students.java
@@ -0,0 +1,9 @@
+package ru.IzumIzBulki.server.model;
+import java.util.List;
+
+/**
+ * Клас студенты, хранит список всех студентов из базы данных в памяти
+ */
+public class Students {
+    List<Student> results;
+}
