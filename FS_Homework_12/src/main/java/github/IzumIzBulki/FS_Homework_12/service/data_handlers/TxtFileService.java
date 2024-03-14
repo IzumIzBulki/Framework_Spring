08d3d292d940827c0ff3e4ba9a0b9c5c4fb1c9f2
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-03-14 14:59:22 +0300

    Create TxtFileService.java

---
+++ FS_Homework_12/src/main/java/github/IzumIzBulki/FS_Homework_12/service/data_handlers/TxtFileService.java
@@ -0,0 +1,13 @@
+package github.IzumIzBulki.FS_Homework_12.service.data_handlers;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+
+@Service("txtWriter")
+@MessagingGateway(defaultRequestChannel = "textInputChanel")
+public interface TxtFileService extends DataService{
+    @Override
+    void writeData(@Header(FileHeaders.FILENAME) String filename, String data);
+}
